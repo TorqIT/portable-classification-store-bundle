0ca959a9dc29f09abb25bc0e2abf51d92c592002
cameronfromtorq <105230931+cameronfromtorq@users.noreply.github.com>
2025-08-08 13:05:26 -0300

    rename exported file

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -3,6 +3,7 @@
 namespace TorqIT\TorqITPortableClassificationStoreBundle\Controller;
 
 use Pimcore\Bundle\AdminBundle\Controller\AdminAbstractController;
+use Pimcore\Model\DataObject\Classificationstore\StoreConfig;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use TorqIT\TorqITPortableClassificationStoreBundle\Services\ExportStoreService;
@@ -13,12 +14,15 @@ class ExportController extends AdminAbstractController
     #[Route("/{classificationstoreId}", name: "pimcore_bundle_portalclassificationstore_export", methods: ["GET"])]
     public function getExportAction(int $classificationstoreId, ExportStoreService $exportStoreService): Response
     {
-        $exportData =
-            $exportStoreService->generateStoreData($classificationstoreId);
+        if (!$store = StoreConfig::getById($classificationstoreId)) {
+            return $this->json(['error' => 'Classification store not found'], Response::HTTP_NOT_FOUND);
+        }
+        $storeName = $store->getName();
+        $exportData = $exportStoreService->generateStoreData($classificationstoreId);
 
         // Set headers to force download
         header('Content-Type: application/json');
-        header('Content-Disposition: attachment; filename="classificationstore_data.json"');
+        header('Content-Disposition: attachment; filename="' . $storeName . '.sql"');
         header('Content-Length: ' . strlen($exportData));
 
         // Output the JSON data as a file
